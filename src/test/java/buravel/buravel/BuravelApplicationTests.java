1c1805a8dc9bfb3233ef552f2f18db88a0f719cd
skarltjr <kisa0828@naver.com>
2021-04-29 01:05:47 +0900

    basic settings

---
+++ src/test/java/buravel/buravel/BuravelApplicationTests.java
@@ -0,0 +1,13 @@
+package buravel.buravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
