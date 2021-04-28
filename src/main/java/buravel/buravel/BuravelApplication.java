ee63e5a38e644af27a1b28a3cbf25808e0c5aa99
skarltjr <kisa0828@naver.com>
2021-04-29 01:05:47 +0900

    basic settings

---
+++ src/main/java/buravel/buravel/BuravelApplication.java
@@ -0,0 +1,13 @@
+package buravel.buravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuravelApplication.class, args);
+    }
+
+}
