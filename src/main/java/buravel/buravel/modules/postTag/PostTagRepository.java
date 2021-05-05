5e1a1ddbc4bb8610b1d3b1b3dd498fb2e517bc64
eunsoo <eunsoo999@naver.com>
2021-05-05 18:23:06 +0900

    [ADD] PATCH /plans 플랜 수정하기

--- src/main/java/buravel/buravel/modules/postTag/PostTagRepository.java
+++ src/main/java/buravel/buravel/modules/postTag/PostTagRepository.java
@@ -1,8 +1,10 @@
 package buravel.buravel.modules.postTag;
 
+import buravel.buravel.modules.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
 public interface PostTagRepository extends JpaRepository<PostTag,Long> {
+    void deleteByPost(Post beforePost);
 }
