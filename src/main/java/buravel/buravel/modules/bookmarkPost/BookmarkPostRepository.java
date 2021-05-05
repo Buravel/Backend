01ba6afbeea54446835b9e729d6cba070461338a
YeEun Lee <37532204+RulLu16@users.noreply.github.com>
2021-05-05 23:46:36 +0900

    [ADD] GET /bookmark/{bookmarkId} 북마크 포스트 리스트 가져오기.  (#20)
    * [ADD] bookmarkpost 기본 틀
    
    * [ADD] Get/bookmark/bookmarkid bookmarkpost 가져오기 구현. query 수정 필요할수도
    
    * [ADD] Get/bookmark/id bookmarkpostdtos 가져오기. responsedto적용
    
    * [FIX] noSuchElementException fix
    
    * [FIX] minor fix

--- src/main/java/buravel/buravel/modules/bookmarkPost/BookmarkPostRepository.java
+++ src/main/java/buravel/buravel/modules/bookmarkPost/BookmarkPostRepository.java
@@ -1,8 +1,14 @@
 package buravel.buravel.modules.bookmarkPost;
 
+import buravel.buravel.modules.bookmark.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional(readOnly = true)
 public interface BookmarkPostRepository extends JpaRepository<BookmarkPost,Long> {
+
+    List<BookmarkPost> findByBookmarkAndChecked(Bookmark bookmark, boolean checked);
+    // todo: spring data jpa
 }
--- src/main/java/buravel/buravel/modules/bookmarkPost/BookmarkPostService.java
+++ src/main/java/buravel/buravel/modules/bookmarkPost/BookmarkPostService.java
@@ -1,11 +1,65 @@
 package buravel.buravel.modules.bookmarkPost;
 
+import buravel.buravel.modules.bookmark.Bookmark;
+import buravel.buravel.modules.bookmark.BookmarkRepository;
+import buravel.buravel.modules.post.Post;
+import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class BookmarkPostService {
+
+    private final BookmarkPostRepository bookmarkPostRepository;
+    private final BookmarkRepository bookmarkRepository;
+    private final ModelMapper modelMapper;
+
+    public List<BookmarkPostResponseDto> getBookmarkPosts(Long bookmarkId) {
+        Optional<Bookmark> bookmarkEntity = bookmarkRepository.findById(bookmarkId);
+
+        if(bookmarkEntity.isEmpty()){
+            return null;
+        } // no such bookmark folder
+
+        Bookmark bookmark = bookmarkEntity.get();
+
+        List<BookmarkPost> bookmarkPostList = bookmarkPostRepository.findByBookmarkAndChecked(bookmark, false);
+        List<BookmarkPostResponseDto> bookmarkPostResponseDtos = new ArrayList<>();
+
+        for(BookmarkPost bookmarkPost : bookmarkPostList){
+            bookmarkPostResponseDtos.add(createBookmarkPostResponseDto(bookmarkPost));
+        }
+
+        return bookmarkPostResponseDtos;
+    }
+
+    public BookmarkPostResponseDto createBookmarkPostResponseDto(BookmarkPost bookmarkPost){
+        BookmarkPostResponseDto bookmarkPostResponseDto = modelMapper.map(bookmarkPost, BookmarkPostResponseDto.class);
+
+        if(bookmarkPost.isChecked()){
+            bookmarkPostResponseDto.setPlanOf_id(bookmarkPost.getPlanOf().getId());
+        }
+
+        bookmarkPostResponseDto.setPostBookmarkPostResponseDto(
+                createPostBookmarkPostResponseDto(bookmarkPost.getPost()));
+
+        bookmarkPostResponseDto.setBookmark_id(bookmarkPost.getBookmark().getId());
+
+        return bookmarkPostResponseDto;
+    }
+
+    public PostBookmarkPostResponseDto createPostBookmarkPostResponseDto(Post post){
+        PostBookmarkPostResponseDto dto = modelMapper.map(post, PostBookmarkPostResponseDto.class);
+
+        dto.setOriginPost_id(post.getId());
+        return dto;
+    }
 }
