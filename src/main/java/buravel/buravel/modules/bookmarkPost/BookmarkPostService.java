2fd6414f6f92d00184b919b615dfb97083c6cd00
YeEun Lee <37532204+RulLu16@users.noreply.github.com>
2021-05-08 09:45:14 +0900

    [ADD] Delete /bookmark/post/bookmarkpostid/postid 북마크 포스트 삭제 추가. (#25)
    * [ADD] bookmarkpost add, delete basic frame
    
    * [FIX] Delete/bookmark/post. delete 추가. 검사 코드 추가 필요
    
    * [ADD] Delete /bookmark/post/bookmarkpostid/postid 북마크 포스트 삭제 추가.

--- src/main/java/buravel/buravel/modules/bookmarkPost/BookmarkPostService.java
+++ src/main/java/buravel/buravel/modules/bookmarkPost/BookmarkPostService.java
@@ -3,11 +3,13 @@ package buravel.buravel.modules.bookmarkPost;
 import buravel.buravel.modules.bookmark.Bookmark;
 import buravel.buravel.modules.bookmark.BookmarkRepository;
 import buravel.buravel.modules.post.Post;
+import buravel.buravel.modules.post.PostRepository;
 import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +22,7 @@ public class BookmarkPostService {
 
     private final BookmarkPostRepository bookmarkPostRepository;
     private final BookmarkRepository bookmarkRepository;
+    private final PostRepository postRepository;
     private final ModelMapper modelMapper;
 
     public List<BookmarkPostResponseDto> getBookmarkPosts(Long bookmarkId) {
@@ -62,4 +65,45 @@ public class BookmarkPostService {
         dto.setOriginPost_id(post.getId());
         return dto;
     }
+
+    public BookmarkPostResponseDto addBookmarkPosts(Long bookmarkId, Long postId){
+        BookmarkPostResponseDto bookmarkPostResponseDto = new BookmarkPostResponseDto();
+
+        return bookmarkPostResponseDto;
+    }
+
+    public boolean processDeleteBookmarkPost(Long bookmarkPostId, Long postId){
+        Optional<BookmarkPost> bookmarkPostEntity = bookmarkPostRepository.findById(bookmarkPostId);
+        if(bookmarkPostEntity.isEmpty()){
+            return false;
+        } // no such bookmark post
+
+        BookmarkPost bookmarkPost = bookmarkPostEntity.get();
+
+        if(bookmarkPost.getPost().getId() != postId){
+            return false;
+        } // postId 잘못 들어옴. todo: 필요한가? postId 받을 필요가 없어보임. 오히려 null 잘못받으면 에러날 듯
+
+        return deleteBookmarkPost(bookmarkPost);
+    }
+
+    public boolean deleteBookmarkPost(BookmarkPost bookmarkPost){
+        if(bookmarkPost.isChecked()){
+            return false;
+        } // there is a plan that uses this bookmark post.
+
+        Optional<Bookmark> bookmarkEntity = bookmarkRepository.findById(bookmarkPost.getBookmark().getId());
+        if(!bookmarkEntity.isEmpty()){
+            bookmarkEntity.get().getBookmarkPosts().remove(bookmarkPost);
+        } // 양방향 북마크 매핑 삭제
+
+        Optional<Post> postEntity = postRepository.findById(bookmarkPost.getPost().getId());
+        if(!postEntity.isEmpty()){
+            postEntity.get().getBookmarkPosts().remove(bookmarkPost);
+        } // 양방향 포스트 매핑 삭제
+
+        bookmarkPostRepository.deleteById(bookmarkPost.getId());
+        return true;
+    }
+    
 }
