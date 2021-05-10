04abc2d824a837a6c44cf07fd5115f19623a1599
skarltjr <kisa0828@naver.com>
2021-05-10 22:29:52 +0900

    [FIX] - patch/plans 빠진 dto 추가

---
+++ src/main/java/buravel/buravel/modules/plan/PlanPatchDto.java
@@ -0,0 +1,36 @@
+package buravel.buravel.modules.plan;
+
+
+import buravel.buravel.modules.post.PostDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlanPatchDto {
+
+    private Long id;
+
+    @NotBlank
+    private String planTitle;
+    private String planImage;
+    @NotNull
+    private boolean published;
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+    private String planTag;
+
+    private PostDto[][] postDtos;
+}
+
