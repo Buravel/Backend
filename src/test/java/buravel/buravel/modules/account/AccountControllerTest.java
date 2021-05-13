e14b31dee3fa7b90f2f38330cf8daa70195026ea
skarltjr <kisa0828@naver.com>
2021-05-14 03:00:02 +0900

    [IFX] - testcode  실패 상황 보충.

--- src/test/java/buravel/buravel/modules/account/AccountControllerTest.java
+++ src/test/java/buravel/buravel/modules/account/AccountControllerTest.java
@@ -23,8 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @Transactional
@@ -70,6 +69,28 @@ class AccountControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk());
     }
+    @Test
+    @DisplayName("회원가입 에러")
+    void signUp_wrong() throws Exception {
+        Account kiseok = Account.builder()
+                .username("kiseok")
+                .password(passwordEncoder.encode("123456789"))
+                .email("kisa0828@naver.com")
+                .build();
+        accountRepository.save(kiseok);
+
+        AccountDto accountDto = new AccountDto();
+        accountDto.setUsername("kiseok");
+        accountDto.setPassword("123456789");
+        accountDto.setEmail("kisa0828@naver.com");
+        accountDto.setNickname("hello");
+
+        mockMvc.perform(post("/signUp")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(accountDto)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("errors").exists());
+    }
 
     @Test
     @DisplayName("로그인 정상")
@@ -94,7 +115,7 @@ class AccountControllerTest {
 
     @Test
     @DisplayName("임시 비밀번호 발급")
-    void loginWithTempPassword()throws Exception {
+    void getTempPassword()throws Exception {
         AccountDto accountDto = new AccountDto();
         accountDto.setUsername("kiseok");
         accountDto.setEmail("kisa0828@naver.com");
@@ -110,6 +131,21 @@ class AccountControllerTest {
         assertThat(user.getPassword().matches("123456789")).isFalse();
 
     }
+    @Test
+    @DisplayName("임시 비밀번호 발급 에러 - 이메일 인증을 한 회원만 가능")
+    void getTempPassword_wrong() throws Exception {
+        AccountDto accountDto = new AccountDto();
+        accountDto.setUsername("kiseok");
+        accountDto.setEmail("kisa0828@naver.com");
+        accountDto.setPassword("123456789");
+        accountDto.setNickname("hello");
+        Account account = accountService.processNewAccount(accountDto);
+        account.setEmailVerified(false);
+
+        mockMvc.perform(post("/tempPassword")
+                .param("email", "kisa0828@naver.com"))
+                .andExpect(status().isForbidden());
+    }
 
     @Test
     @DisplayName("이메일 인증 성공")
@@ -121,6 +157,18 @@ class AccountControllerTest {
                 .header(HttpHeaders.AUTHORIZATION, token)
                 .param("token", emailCheckToken))
                 .andExpect(status().isOk());
+    }
+    @Test
+    @DisplayName("이메일 인증 실패")
+    void emailVerification_fail()throws Exception {
+        String token = getAccessToken();
+        Account user = accountRepository.findByUsername("kiseok");
+        String emailCheckToken = user.getEmailCheckToken();
+        mockMvc.perform(post("/emailVerification")
+                .header(HttpHeaders.AUTHORIZATION, token)
+                .param("token", emailCheckToken+"error"))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 
     private String getAccessToken() throws Exception {
