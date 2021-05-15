a5de7179fcea2c371155a9881a45423242d0083d
skarltjr <kisa0828@naver.com>
2021-05-16 01:41:59 +0900

    [FIX] Post/emailCheckToken - 인증 토큰 재발급도 eventPublisher + Async를 통해 처리하도록 수정

--- src/main/java/buravel/buravel/modules/plan/PlanResource.java
+++ src/main/java/buravel/buravel/modules/plan/PlanResource.java
@@ -2,6 +2,7 @@ package buravel.buravel.modules.plan;
 
 import buravel.buravel.modules.IndexController;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -11,6 +12,7 @@ public class PlanResource extends EntityModel<PlanResponseDto> {
     public static EntityModel<PlanResponseDto> modelOf(PlanResponseDto planResponseDto) {
         EntityModel<PlanResponseDto> planResource = EntityModel.of(planResponseDto);
         planResource.add(linkTo(methodOn(IndexController.class).index()).withRel("index"));
+        planResource.add(Link.of("https://docs.google.com/spreadsheets/d/1XmJZD9VyPquyhDm9XyfJL_KO89xLlN5-VTQGh6MTW-g/edit#gid=0").withRel("profile"));
         return planResource;
     }
 }
