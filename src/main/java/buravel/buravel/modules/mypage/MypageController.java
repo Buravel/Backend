73eb0cd1702a1068cba214548cb0e3dec0efd0ce
eunsoo <eunsoo999@naver.com>
2021-05-16 20:07:13 +0900

    [ADD] GET, PATCH /mypage 정보조회, 수정하기

--- src/main/java/buravel/buravel/modules/mypage/MypageController.java
+++ src/main/java/buravel/buravel/modules/mypage/MypageController.java
@@ -2,20 +2,23 @@ package buravel.buravel.modules.mypage;
 
 import buravel.buravel.modules.account.Account;
 import buravel.buravel.modules.account.CurrentUser;
+import buravel.buravel.modules.errors.ErrorResource;
+import buravel.buravel.modules.mypage.validator.UpdateUserNicknameValidator;
 import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 public class MypageController {
 
     private final MypageService mypageService;
+    private final UpdateUserNicknameValidator validator;
 
     @PatchMapping("/mypage/picture")
     public ResponseEntity updateUserPicture(@RequestBody PictureRequestDto pictureRequestDto, @CurrentUser Account account){
@@ -31,4 +34,37 @@ public class MypageController {
 
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("/mypage")
+    public ResponseEntity getUserinfo(@CurrentUser Account account) {
+        UserInfoResponseDto userInfo = mypageService.getUserInfo(account);
+        return ResponseEntity.ok(userInfo);
+    }
+
+    @PatchMapping("/mypage/nickname")
+    public ResponseEntity updateUserNickname(@RequestBody @Valid UserNicknameRequestDto userNicknameRequestDto, @CurrentUser Account account, Errors errors) {
+        if(errors.hasErrors()){
+            EntityModel<Errors> error1 = ErrorResource.modelOf(errors);
+            return ResponseEntity.badRequest().body(error1);
+        }
+        validator.validate(userNicknameRequestDto, account, errors);
+        if(errors.hasErrors()){
+            EntityModel<Errors> error2 = ErrorResource.modelOf(errors);
+            return ResponseEntity.badRequest().body(error2);
+        }
+
+        UserInfoResponseDto userInfo = mypageService.updateUserNickname(account, userNicknameRequestDto);
+        return ResponseEntity.ok(userInfo);
+    }
+
+    @PatchMapping("/mypage/password")
+    public ResponseEntity updateUserPassword(@RequestBody @Valid UserPasswordRequestDto userPasswordRequestDto, @CurrentUser Account account, Errors errors) {
+        if(errors.hasErrors()){
+            EntityModel<Errors> error1 = ErrorResource.modelOf(errors);
+            return ResponseEntity.badRequest().body(error1);
+        }
+        UserInfoResponseDto userInfo = mypageService.updateUserPassword(account, userPasswordRequestDto);
+        return ResponseEntity.ok(userInfo);
+    }
+
 }
---
+++ src/main/java/buravel/buravel/modules/mypage/validator/UpdateUserNicknameValidator.java
@@ -0,0 +1,34 @@
+package buravel.buravel.modules.mypage.validator;
+
+import buravel.buravel.modules.account.Account;
+import buravel.buravel.modules.account.AccountRepository;
+import buravel.buravel.modules.mypage.UserNicknameRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+public class UpdateUserNicknameValidator implements Validator {
+    private final AccountRepository accountRepository;
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return aClass.isAssignableFrom(UpdateUserNicknameValidator.class);
+    }
+    @Override
+    public void validate(Object target, Errors errors) { }
+
+    public void validate(Object object, Account account, Errors errors) {
+        UserNicknameRequestDto form = (UserNicknameRequestDto) object;
+        Account current = accountRepository.findById(account.getId()).get();
+        Account updateUser = accountRepository.findByNickname(form.getNickname());
+
+        if (current.equals(updateUser)) {
+            errors.rejectValue("nickname","same nickname","현재 닉네임과 동일합니다.");
+        } else if (updateUser != null) {
+            errors.rejectValue("nickname","invalid nickname",new Object[]{form.getNickname()},"이미 사용중인 닉네임입니다.");
+        }
+    }
+}
+
