99744fc57005d61dd99adb08f66ee0e1212f62ba
eunsoo <eunsoo999@naver.com>
2021-05-20 02:20:37 +0900

    [ADD] /mypage profile 링크 추가

--- src/main/java/buravel/buravel/modules/mypage/MypageResource.java
+++ src/main/java/buravel/buravel/modules/mypage/MypageResource.java
@@ -3,6 +3,7 @@ package buravel.buravel.modules.mypage;
 import buravel.buravel.modules.IndexController;
 import buravel.buravel.modules.plan.PlanController;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -16,6 +17,7 @@ public class MypageResource extends EntityModel<UserInfoResponseDto> {
         userInfoResource.add(linkTo(PlanController.class).slash("mine").slash("closed").withRel("getMyClosedPlans"));
         userInfoResource.add(linkTo(PlanController.class).slash("mine").slash("published").withRel("getMyPublishedPlans"));
         userInfoResource.add(linkTo(MypageController.class).slash("account").withRel("deleteAccount"));
+        userInfoResource.add(Link.of("https://docs.google.com/spreadsheets/d/1XmJZD9VyPquyhDm9XyfJL_KO89xLlN5-VTQGh6MTW-g/edit#gid=0").withRel("profile"));
 
         return userInfoResource;
     }
